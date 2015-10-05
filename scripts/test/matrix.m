73819f771761cc59bca6371a79b7c0f873ea5c49
Jernej Kovacic <jkovacic@gmail.com>
2015-10-05 11:00:45 +0200

    Matlab style of printf in .m scripts

--- scripts/test/matrix.m
+++ scripts/test/matrix.m
@@ -8,66 +8,66 @@
 %   matrix
 
 f = [1, 0.5, 4.5; 0, 1, 0.4]
-printf("f(0,1) = %f\n", f(0+1, 1+1));
-printf("f(4) = %f\n", f(4+1));
+printf('f(0,1) = %f\n', f(0+1, 1+1));
+printf('f(4) = %f\n', f(4+1));
 
-printf("\nf multiplied by 3:\n");  3 * f
-printf("f conjugated:\n");  conj(f)
-printf("f multiplied by 0.5:\n");  f * 0.5
+printf('\nf multiplied by 3:\n');  3 * f
+printf('f conjugated:\n');  conj(f)
+printf('f multiplied by 0.5:\n');  f * 0.5
 
-printf("t = f transposed:\n");
+printf('t = f transposed:\n');
 t = f'
 
-printf("t * f:\n");  t * f
-printf("f * t:\n");  f * t
+printf('t * f:\n');  t * f
+printf('f * t:\n');  f * t
 
-printf("3x3 unit matrix:\n")
+printf('3x3 unit matrix:\n')
 a = eye(3)
 m1 = [1, 2.4, -1.4, 0; 4.5, 1, 0, -0.5; 0, 1.75, 1, 2]
-printf("removed the 3rd column from m1:\n");
+printf('removed the 3rd column from m1:\n');
 m1(:, 3) = []
-printf("inserted a column of zeros between the 1st and the 2nd column of m1:\n");
+printf('inserted a column of zeros between the 1st and the 2nd column of m1:\n');
 m1 = [m1(:, 1:1), zeros(3, 1), m1(:, 2:end)]
-printf("removed the 2nd row of m1\n");
+printf('removed the 2nd row of m1\n');
 m1(2, :) = []
-printf("\inserted a row (zeroes) between the 1st and 2nd row of m1\n");
+printf('\inserted a row (zeroes) between the 1st and 2nd row of m1\n');
 m1 = [m1(1:1, :); zeros(1, 4); m1(2:end, :)]
-printf("previous matrix transposed:\n");
+printf('previous matrix transposed:\n');
 m1'
 
 sq = [1, 2; 3, 4]; 
 kv = sq
-printf("-kv:\n");  -kv
+printf('-kv:\n');  -kv
 m3 = -kv;
 
 a1 = [1, 2, 3; 4, 5, 6; 7, 9, 8]
-printf("Determinant of a1: %f\n", det(a1));
-printf("\ninverse of a1:\n");
+printf('Determinant of a1: %f\n', det(a1));
+printf('\ninverse of a1:\n');
 invv = inv(a1)
-printf("a1 * inv   (must be a unit matrix):\n");  a1 * invv
+printf('a1 * inv   (must be a unit matrix):\n');  a1 * invv
 
-printf("Upper triangular part (incl. diag) of a1:\n");
+printf('Upper triangular part (incl. diag) of a1:\n');
 triu(a1)
-printf("Lower triangular part (excl. diag) of a1:\n");
+printf('Lower triangular part (excl. diag) of a1:\n');
 tril(a1, -1)
-printf("Diagonal part of a1:\n");
+printf('Diagonal part of a1:\n');
 m1 = diag(diag(a1))
 
-printf("invv transposed:\n");
+printf('invv transposed:\n');
 invv = invv'
-printf("Add 0.5 to inv(2, 0):\n");
+printf('Add 0.5 to inv(2, 0):\n');
 invv(2+1, 0+1) = invv(2+1, 0+1) + 0.5
-printf("Swap the 2nd and the 3rd row:\n");
+printf('Swap the 2nd and the 3rd row:\n');
 invv([2, 3], :) = invv([3, 2], :)
-printf("Swap the 2nd and the 3rd column:\n");
+printf('Swap the 2nd and the 3rd column:\n');
 invv(:, [2, 3]) = invv(:, [3, 2])
 
 
 c1 = [1+i, 1-2i; 2-3i, 2+4i]
-printf("c1 conjugated:\n");
+printf('c1 conjugated:\n');
 conj(c1)
 fm = [1.1, 2.2, 3.3, 4.4]'
-printf("fm transposed:\n");
+printf('fm transposed:\n');
 fmt = fm'
-printf("fm transposed transposed:\n");
+printf('fm transposed transposed:\n');
 fmt = fmt'
--- scripts/test/quat.m
+++ scripts/test/quat.m
@@ -14,43 +14,43 @@
 pkg load quaternion
 
 zeroq = quaternion(0);
-printf("Zero: "); zeroq
+printf('Zero: '); zeroq
 q = quaternion(1.0, -0.5, 1.2, -2.3)
-printf("q: 1:%f   i:%f   j:%f   k:%f\n", ...
+printf('q: 1:%f   i:%f   j:%f   k:%f\n', ...
     get(q, 'w'), get(q, 'x'), get(q, 'y'), get(q, 'z') );
-printf("abs(q): %f\n", abs(q));
+printf('abs(q): %f\n', abs(q));
 qu = unit(q);
-printf("Unit quaternion of q: ");  qu
-printf("norm(qu): %f\n", norm(qu));
+printf('Unit quaternion of q: ');  qu
+printf('norm(qu): %f\n', norm(qu));
 
 o = quaternion(1);
 i = qi();
 j = qj();
 k = qk();
-printf("i*j: ");  i * j
-printf("j*i: ");  j * i
-printf("j*k: ");  j * k
-printf("k*j: ");  k * j
-printf("k*i: ");  k * i
-printf("i*k: ");  i * k
+printf('i*j: ');  i * j
+printf('j*i: ');  j * i
+printf('j*k: ');  j * k
+printf('k*j: ');  k * j
+printf('k*i: ');  k * i
+printf('i*k: ');  i * k
 
 p = quaternion(1, 2, 3, 4)
 q
-printf("p+q: ");  p + q
-printf("p-q: ");  p - q
-printf("p*q: ");  p * q
-printf("q*p: ");  q * p
-printf("p+5: ");  p + 5
-printf("3+p: ");  3 + p
-printf("p-4: ");  p - 4
-printf("1-p: ");  1 - p
-printf("p+2: ");  p += 2
-printf("p+2-2: ");  p -= 2
+printf('p+q: ');  p + q
+printf('p-q: ');  p - q
+printf('p*q: ');  p * q
+printf('q*p: ');  q * p
+printf('p+5: ');  p + 5
+printf('3+p: ');  3 + p
+printf('p-4: ');  p - 4
+printf('1-p: ');  1 - p
+printf('p+2: ');  p += 2
+printf('p+2-2: ');  p -= 2
 
 qc = -0.5 * (q +i*q*i + j*q*j + k*q*k);
-printf("-0.5*(q+i*q*i+j*q*j+k*q*k): ");  qc
-printf("conj(q): ");  conj(q)
+printf('-0.5*(q+i*q*i+j*q*j+k*q*k): ');  qc
+printf('conj(q): ');  conj(q)
 
 qrec = inv(q)
-printf("q * qrec: ");  q * qrec
-printf("qrec * q: ");  qrec * q
+printf('q * qrec: ');  q * qrec
+printf('qrec * q: ');  qrec * q
