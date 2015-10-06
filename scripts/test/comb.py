755ac35cf96bb36cc6babdc9c97d3957c69dd5ed
Jernej Kovacic <jkovacic@gmail.com>
2015-10-06 09:07:28 +0200

    compatibility with Python3

--- scripts/test/comb.py
+++ scripts/test/comb.py
@@ -5,24 +5,28 @@
 #   python /path/to/comb.py
 #
 # From Python, the script can be run as:
-#   import comb
+#   execfile("/path/to/comb.py")
+# from Python 2 or
+#   exec(open("/path/to/comb.py").read())
+# from Python 3
 
 
+from __future__ import print_function
 import itertools as it
 
 perm = it.permutations("abcde")
 
-print "Permutations...\n"
+print("Permutations...\n")
 cnt = 1
 for i in list(perm) :
-    print cnt, ": ", ''.join(i)
+    print(cnt, ": ", ''.join(i))
     cnt += 1
 
-print "\nCombinations...\n"
+print("\nCombinations...\n")
 for k in range(1, 6) :
-    print "\nK = ", k, "\n"
+    print("\nK = ", k, "\n")
     comb = it.combinations("abcde", k)
     cnt = 1
     for i in list(comb) :
-        print cnt, ": ", ''.join(i)
+        print(cnt, ": ", ''.join(i))
         cnt += 1
